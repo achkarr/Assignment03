0b2edbb05b1bb6e3e07a51979caf8d0bc062d420
LunarTechnology <61947976+LunarTechnology@users.noreply.github.com>
2021-04-03 10:05:56 -0400

    AddBcc Test
    Added AddBcc test case

---
+++ src/test/java/org/apache/commons/mail/EmailTest.java
@@ -0,0 +1,41 @@
+package org.apache.commons.mail;
+
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.EmptyStackException;
+
+import org.junit.After;
+import org.junit.Test;
+import org.subethamail.smtp.server.Session;
+import java.util.Date;
+
+public class EmailTest {
+	
+	private static final String[] TestEmails = {"ab@bc.com", "a.b@c.org", "test@gmail.com"};
+
+	private EmailConcrete email;
+	@Before
+	public void setUpEmailTest() throws Exception {
+		
+		email = new EmailConcrete();
+		
+	}
+	
+	@After
+	public void tearDownEmailTest() throws Exception 
+	{
+		
+	}
+	
+	@Test
+	public void testaddBcc() throws Exception
+	{
+		email.addBcc(TestEmails);
+		assertEquals(3, email.bccList.size());
+		assertEquals(3, email.getBccAddresses().size());
+	}
+	
+	
+}
